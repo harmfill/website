219d8a189a76325e42ae4b51bc93982eaf0e1625
harmfill <peenpeen7452698@gmail.com>
2025-03-30 08:33:16 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+function typeText(element, text, speed) {
+    let index = 0;
+    function type() {
+        if (index < text.length) {
+            element.textContent += text.charAt(index);
+            index++;
+            setTimeout(type, speed);
+        }
+    }
+    type();
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    const textElement = document.querySelector(".click-to-enter-text");
+    const textToType = "Click to Enter...";
+    textElement.textContent = "";
+    typeText(textElement, textToType, 150);
+});
+
 function enterSite() {
     const clickToEnter = document.querySelector('.click-to-enter');
     clickToEnter.classList.add('fadeOut');
